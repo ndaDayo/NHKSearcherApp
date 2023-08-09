e2e6d8f433d3e53c3bb6bb69128fef126f1354e1
ndaDayo <hayato711+nda@gmail.com>
2023-08-09 21:41:19 +0900

    Initial Commit

---
+++ NHKSearcher/NHKSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NHKSearcherApp.swift
+//  NHKSearcher
+//
+//  Created by hayato sugawara on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct NHKSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
