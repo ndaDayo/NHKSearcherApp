87e23ca17d2362c48b6133250afd0c611680cf40
ndaDayo <hayato711+nda@gmail.com>
2023-08-09 21:50:32 +0900

    [add] ProgramSearchView

---
+++ NHKSearcher/View/ProgramSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgramSearchView.swift
+//  NHKSearcher
+//
+//  Created by hayato sugawara on 2023/08/09.
+//
+
+import SwiftUI
+
+struct ProgramSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgramSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgramSearchView()
+    }
+}
